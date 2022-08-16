c4f3b9dfdc5d47ef83761699f7292f4ac8cb9b3b
bczernecki <bczernecki@gmail.com>
2022-08-16 16:42:58 +0200

    polska wiatr

---
+++ R/31_era5_hiszpania_wiatr_postprocessing.R
@@ -0,0 +1,69 @@
+#### Compute risk of wind gust based on ERA-5 dataset ####
+
+# cdo formulas:
+# cdo -b F64 -mergetime poryw*.nc calosc.nc
+# cdo yearpctl,99 calosc.nc -yearmin calosc.nc -yearmax calosc.nc calosc_p99.nc
+# cdo yearmax calosc poryw_ymax.nc
+# cdo yearsum -gec,15 calosc.nc poryw_over_15.nc
+# cdo yearsum -gec,20 calosc.nc poryw_over_20.nc
+# cdo yearsum -gec,25 calosc.nc poryw_over_25.nc
+# cdo yearsum -gec,30 calosc.nc poryw_over_30.nc
+
+
+setwd("~/github/kinga-grad/data/hiszpania_wiatr/")
+library(raster)
+library(terra)
+library(sf)
+
+# get boundaries
+fr = getData(name = "GADM", country = "ESP", level = 1)
+fr = st_as_sf(fr)
+fr = fr[-13:-14,]
+fr = dplyr::select(fr, NAME_1)
+fr_buffer = st_buffer(fr, dist = 10000)
+fr_buffer = terra::aggregate(terra::vect(fr_buffer), dissolve = TRUE)
+
+fr = terra::vect(fr)
+plot(fr)
+
+# srednia liczba dni w roku z przekroczeniem porywu 20 m/s:
+poryw_over_20 = terra::mask(mean(rast("poryw_over_20.nc")), fr_buffer)
+plot(poryw_over_20)
+plot(fr, add = TRUE)
+raster::writeRaster(poryw_over_20, filename = "poryw_over_20.tif", overwrite = TRUE)
+
+# srednia liczba dni w roku z przekroczeniem porywu 25 m/s:
+poryw_over_25 = terra::mask(mean(rast("poryw_over_25.nc")), fr_buffer)
+plot(poryw_over_25)
+plot(fr, add = TRUE)
+raster::writeRaster(poryw_over_25, filename = "poryw_over_25.tif" , overwrite = TRUE)
+
+# srednia liczba dni w roku z przekroczeniem porywu 30 m/s:
+poryw_over_30 = terra::mask(mean(rast("poryw_over_30.nc")), fr_buffer)
+plot(poryw_over_30)
+plot(fr, add = TRUE)
+raster::writeRaster(poryw_over_30, filename = "poryw_over_30.tif", overwrite = TRUE)
+
+# sredni max roczny:
+wind_max = terra::mask(mean(rast("poryw_ymax.nc")), fr_buffer) * 1.03
+plot(wind_max)
+plot(fr, add = TRUE)
+raster::writeRaster(wind_max, filename = "poryw_avg_annual_max.tif", overwrite = TRUE)
+
+# historyczny max:
+wind_max2 = terra::mask(max(rast("poryw_ymax.nc")), fr_buffer) * 1.03
+plot(wind_max2)
+plot(fr, add = TRUE)
+raster::writeRaster(wind_max2, filename = "poryw_max_record.tif", overwrite = TRUE)
+
+# max roczny o prawdpodobienstwa co 10 lat:
+wind_max_10lat = terra::mask(quantile(rast("poryw_ymax.nc"), 0.9), fr_buffer)*1.03
+plot(wind_max_10lat)
+plot(fr, add = TRUE)
+raster::writeRaster(wind_max_10lat, filename = "poryw_max_na_10_lat.tif", overwrite = TRUE)
+
+# kwantyl 99 maksymalnej rocznej predkosci wiatru
+wind_q99 = terra::mask(quantile(rast("calosc_p99.nc"), 0.9), fr_buffer)*1.03
+plot(wind_q99)
+plot(fr, add = TRUE)
+raster::writeRaster(wind_q99, filename = "poryw_roczny_percentyl99.tif", overwrite = TRUE)
---
+++ R/33_draw_maps_polska.R
@@ -0,0 +1,227 @@
+# rysowanie map wiatrowych z wygenerowanych tifow:
+library(terra)
+library(tmap)
+library(sf)
+
+
+# get boundaries
+fr = getData(name = "GADM", country = "POL", level = 1)
+fr = st_as_sf(fr)
+#fr = fr[-which(fr$VARNAME_1 == "Corsica"),]
+fr = dplyr::select(fr, NAME_1)
+#fr = terra::vect(fr)
+plot(fr)
+# zmiana formatu na gadajacy z tmapem:
+fr_bnd2 = sf::st_as_sf(fr)
+fr2 = sf::st_as_sf(fr)
+
+fr_terra = terra::vect(fr)
+
+r = terra::mask(mean(rast("poryw_avg_annual_max.tif")), fr_terra)
+r = raster(r)
+library(tmap)
+# rysowanie Maksymalnego średniego porywu w roku:
+tm_shape(fr_bnd2)+
+  tm_borders() + # just for start to have extent:
+  tm_layout(legend.outside.size = 0.15, legend.outside.position = "right", legend.outside = TRUE,
+            legend.frame = FALSE, frame = FALSE) +
+  tm_layout(title = "Mean annual\n max. wind gust", bg.color = "gray90") +
+  tm_graticules(n.x = 6, n.y = 5, col= "white") +
+  tm_scale_bar(breaks = c(0, 100, 200), position = c("left", "bottom")) +
+  tm_compass(position = c("left", "top"), size = 2) +
+  tm_shape(r) +
+  tm_raster(interpolate = FALSE,
+            title = "[m/s]",
+            #palette = "viridis",
+            pal = c("#E1F5C4", "#EDE574", "#F9D423", "#FC913A", "#FF4E50", "purple"),
+            style = 'pretty',
+            n = 20,
+            legend.is.portrait = TRUE) +
+            #labels = as.character(sprintf("%.2f", seq(0, 1, 0.05)))) +
+  tm_shape(fr_bnd2)+
+  tm_borders() +
+  tm_shape(fr2, col = "black")+
+  tm_polygons(alpha = 0.10) -> p
+
+print(p)
+tmap_save(tm = p, filename = "poryw_avg_annual_max.png")
+
+
+
+
+### rysowanie max. porywu o prawdopodobieństwie ~10 lat
+r = terra::mask(mean(rast("poryw_max_na_10_lat.tif")), fr_terra)
+r = raster(r)
+tm_shape(fr_bnd2)+
+  tm_borders() + # just for start to have extent:
+  tm_layout(legend.outside.size = 0.15, legend.outside.position = "right", legend.outside = TRUE,
+            legend.frame = FALSE, frame = FALSE) +
+  tm_layout(title = "Wind gust\nmax. speed\nper 10 years", bg.color = "gray90") +
+  tm_graticules(n.x = 6, n.y = 5, col= "white") +
+  tm_scale_bar(breaks = c(0, 100, 200), position = c("left", "bottom")) +
+  tm_compass(position = c("left", "top"), size = 2) +
+  tm_shape(r) +
+  tm_raster(interpolate = FALSE,
+            title = "[m/s]",
+            #palette = "viridis",
+            pal = c("#E1F5C4", "#EDE574", "#F9D423", "#FC913A", "#FF4E50", "purple"),
+            style = 'pretty',
+            n = 20,
+            legend.is.portrait = TRUE) +
+  #labels = as.character(sprintf("%.2f", seq(0, 1, 0.05)))) +
+  tm_shape(fr_bnd2)+
+  tm_borders() +
+  tm_shape(fr2, col = "black")+
+  tm_polygons(alpha = 0.10) -> p
+
+print(p)
+tmap_save(tm = p, filename = "poryw_max_na_10_lat.png")
+
+
+
+### rysowanie max. historycznego porywu
+r = terra::mask(mean(rast("poryw_max_record.tif")), fr_terra)
+r = raster(r)
+tm_shape(fr_bnd2)+
+  tm_borders() + # just for start to have extent:
+  tm_layout(legend.outside.size = 0.15, legend.outside.position = "right", legend.outside = TRUE,
+            legend.frame = FALSE, frame = FALSE) +
+  tm_layout(title = "Record high\nwind gusts\n1959-2021", bg.color = "gray90") +
+  tm_graticules(n.x = 6, n.y = 5, col= "white") +
+  tm_scale_bar(breaks = c(0, 100, 200), position = c("left", "bottom")) +
+  tm_compass(position = c("left", "top"), size = 2) +
+  tm_shape(r) +
+  tm_raster(interpolate = FALSE,
+            title = "[m/s]",
+            #palette = "viridis",
+            pal = c("#E1F5C4", "#EDE574", "#F9D423", "#FC913A", "#FF4E50", "purple"),
+            style = 'pretty',
+            n = 20,
+            legend.is.portrait = TRUE) +
+  #labels = as.character(sprintf("%.2f", seq(0, 1, 0.05)))) +
+  tm_shape(fr_bnd2)+
+  tm_borders() +
+  tm_shape(fr2, col = "black")+
+  tm_polygons(alpha = 0.10) -> p
+
+print(p)
+tmap_save(tm = p, filename = "poryw_max_record.png")
+
+
+
+
+### rysowanie sredniej liczby godziny powyzej 20 m/s
+r = terra::mask(rast("poryw_over_20.tif"), fr_terra)
+r = raster(r)
+tm_shape(fr_bnd2)+
+  tm_borders() + # just for start to have extent:
+  tm_layout(legend.outside.size = 0.15, legend.outside.position = "right", legend.outside = TRUE,
+            legend.frame = FALSE, frame = FALSE) +
+  tm_layout(title = "Mean annual\ntime with\nwind gusts\n > 20 m/s", bg.color = "gray90") +
+  tm_graticules(n.x = 6, n.y = 5, col= "white") +
+  tm_scale_bar(breaks = c(0, 100, 200), position = c("left", "bottom")) +
+  tm_compass(position = c("left", "top"), size = 2) +
+  tm_shape(r) +
+  tm_raster(interpolate = FALSE,
+            title = "[hours]",
+            #palette = "viridis",
+            pal = c("#E1F5C4", "#EDE574", "#F9D423", "#FC913A", "#FF4E50", "purple"),
+            style = 'pretty',
+            n = 20,
+            legend.is.portrait = TRUE) +
+  #labels = as.character(sprintf("%.2f", seq(0, 1, 0.05)))) +
+  tm_shape(fr_bnd2)+
+  tm_borders() +
+  tm_shape(fr2, col = "black")+
+  tm_polygons(alpha = 0.10) -> p
+
+print(p)
+tmap_save(tm = p, filename = "poryw_over_20.png")
+
+
+### rysowanie sredniej liczby godziny powyzej 25 m/s
+r = terra::mask(rast("poryw_over_25.tif"), fr_terra)
+r = raster(r)
+tm_shape(fr_bnd2)+
+  tm_borders() + # just for start to have extent:
+  tm_layout(legend.outside.size = 0.15, legend.outside.position = "right", legend.outside = TRUE,
+            legend.frame = FALSE, frame = FALSE) +
+  tm_layout(title = "Mean annual\ntime with\nwind gusts\n > 25 m/s", bg.color = "gray90") +
+  tm_graticules(n.x = 6, n.y = 5, col= "white") +
+  tm_scale_bar(breaks = c(0, 100, 200), position = c("left", "bottom")) +
+  tm_compass(position = c("left", "top"), size = 2) +
+  tm_shape(r) +
+  tm_raster(interpolate = FALSE,
+            title = "[hours]",
+            palette = rev("viridis"),
+            #pal = c("#E1F5C4", "#EDE574", "#F9D423", "#FC913A", "#FF4E50", "purple"),
+            style = 'fixed',
+            breaks = c(0:20/2, 30, 50, 70),
+            legend.is.portrait = TRUE) +
+  #labels = as.character(sprintf("%.2f", seq(0, 1, 0.05)))) +
+  tm_shape(fr_bnd2)+
+  tm_borders() +
+  tm_shape(fr2, col = "black")+
+  tm_polygons(alpha = 0.10) -> p
+
+print(p)
+tmap_save(tm = p, filename = "poryw_over_25.png")
+
+
+
+### rysowanie sredniej liczby godziny powyzej 25 m/s
+r = terra::mask(rast("poryw_over_30.tif"), fr_terra)
+r = raster(r)
+tm_shape(fr_bnd2)+
+  tm_borders() + # just for start to have extent:
+  tm_layout(legend.outside.size = 0.15, legend.outside.position = "right", legend.outside = TRUE,
+            legend.frame = FALSE, frame = FALSE) +
+  tm_layout(title = "Mean annual\ntime with\nwind gusts\n > 30 m/s", bg.color = "gray90") +
+  tm_graticules(n.x = 6, n.y = 5, col= "white") +
+  tm_scale_bar(breaks = c(0, 100, 200), position = c("left", "bottom")) +
+  tm_compass(position = c("left", "top"), size = 2) +
+  tm_shape(r) +
+  tm_raster(interpolate = FALSE,
+            title = "[hours]",
+            palette = rev("viridis"),
+            #pal = c("#E1F5C4", "#EDE574", "#F9D423", "#FC913A", "#FF4E50", "purple"),
+            style = 'fixed',
+            breaks = c(0:30/8, 8, 10),
+            legend.is.portrait = TRUE) +
+  #labels = as.character(sprintf("%.2f", seq(0, 1, 0.05)))) +
+  tm_shape(fr_bnd2)+
+  tm_borders() +
+  tm_shape(fr2, col = "black")+
+  tm_polygons(alpha = 0.10) -> p
+
+print(p)
+tmap_save(tm = p, filename = "poryw_over_30.png")
+
+
+### rysowanie sredniego rocznego percentyla 99
+r = terra::mask(rast("poryw_roczny_percentyl99.tif"), fr_terra)
+r = raster(r)
+tm_shape(fr_bnd2)+
+  tm_borders() + # just for start to have extent:
+  tm_layout(legend.outside.size = 0.15, legend.outside.position = "right", legend.outside = TRUE,
+            legend.frame = FALSE, frame = FALSE) +
+  tm_layout(title = "99th percentile\nof annual\nwind gusts", bg.color = "gray90") +
+  tm_graticules(n.x = 6, n.y = 5, col= "white") +
+  tm_scale_bar(breaks = c(0, 100, 200), position = c("left", "bottom")) +
+  tm_compass(position = c("left", "top"), size = 2) +
+  tm_shape(r) +
+  tm_raster(interpolate = FALSE,
+            title = "[m/s]",
+            #palette = "viridis",
+            pal = c("#E1F5C4", "#EDE574", "#F9D423", "#FC913A", "#FF4E50", "purple"),
+            style = 'pretty',
+            n = 20,
+            legend.is.portrait = TRUE) +
+  #labels = as.character(sprintf("%.2f", seq(0, 1, 0.05)))) +
+  tm_shape(fr_bnd2)+
+  tm_borders() +
+  tm_shape(fr2, col = "black")+
+  tm_polygons(alpha = 0.10) -> p
+
+print(p)
+tmap_save(tm = p, filename = "poryw_roczny_percentyl99.png")
